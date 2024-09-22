8899bfdaa1d61ee7437472c14ca3d9be4072413d
JonEG <echevestegonzalezjon@gmail.com>
2024-09-22 14:54:28 +0200

    red test multiplication

--- test/money_example_test.dart
+++ test/money_example_test.dart
@@ -2,7 +2,9 @@ import 'package:money_example/money_example.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('test multiplication', () {
+    Dollar five = Dollar(5);
+    five.times(2);
+    expect(10, five.amount);
   });
 }
